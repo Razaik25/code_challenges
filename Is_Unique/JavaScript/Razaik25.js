86520f35f92cb217979ba0f2de8953c21af070ee
Razaik Boparai <razaikb@gmail.com>
2016-05-26 19:13:32 -0400

    commented isUnique solution

--- Is_Unique/JavaScript/Razaik25.js
+++ Is_Unique/JavaScript/Razaik25.js
@@ -1,17 +1,18 @@
 
+// ======= Razaik25's Solution ====== //
 function isUnique(input) {
-  if (typeof(input) === 'string') input = input.toLowerCase().split('');
+  if (typeof(input) === 'string') input = input.toLowerCase().split(''); // if input is string split it into an array
   var temp,count = 0;
   for( var i =0; i<input.length; i++){
-    if(temp === input[i]) {
+    if(temp === input[i]) {   // comparing the current value of input with the previous value 
       count++;
       return `${temp} repeats`;
     }
-    temp = input[i];
+    temp = input[i]; // storing the previous value of input
   }
   if (count === 0) return `all characters unique`;
 }
 
 // test cases
-isUnique('hello'); 
+isUnique('hello');
 isUnique([1,2,3]);
